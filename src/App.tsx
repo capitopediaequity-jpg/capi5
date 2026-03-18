f5dbcaf46826fce7546b31fab03298f3a372522a
capitopedia <capitopedia.equity@gmail.com>
2026-03-18 16:56:39 +0530

    Migrate project from Next.js to Vite 5

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import WhyChooseUs from "./components/WhyChooseUs";
+import Stats from "./components/Stats";
+import Process from "./components/Process";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-background selection:bg-primary/30">
+      <Navbar />
+      <Hero />
+      <div className="relative z-10">
+        <Services />
+        <About />
+        <Stats />
+        <WhyChooseUs />
+        <Process />
+        <Testimonials />
+        <Contact />
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
